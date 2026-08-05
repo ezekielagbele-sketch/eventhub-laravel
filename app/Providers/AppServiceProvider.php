10e9fdb4b7d9275ca1020b26ef6f6ae034ed5dbe
Ezekiel Agbele <ezekielagbele@gmail.com>
2026-08-05 16:25:51 +0100

    Force HTTPS and fix asset URLs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,11 +21,11 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        Paginator::useBootstrapFive();
+{
+    Paginator::useBootstrapFive();
 
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
+    if (!App::environment('local')) {
+        URL::forceScheme('https');
     }
+}
 }
