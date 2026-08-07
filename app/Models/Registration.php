46ae0f7951306d935e6c3824ec15c8d757f7c530
Ezekiel Agbele <ezekielagbele@gmail.com>
2026-08-07 09:31:20 +0100

    Restore Registration model

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -1,2 +1,26 @@
+<?php
 
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
+
+class Registration extends Model
+{
+    protected $fillable = [
+        'event_id',
+        'user_id',
+        'name',
+        'email',
+        'phone',
+    ];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
